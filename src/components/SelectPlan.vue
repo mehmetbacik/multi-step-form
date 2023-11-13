4e8f57d63afcd755b2b587be4b1c5e40a046d19f
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-11-13 20:21:22 +0300

    Updated for Select Plan component

--- src/components/SelectPlan.vue
+++ src/components/SelectPlan.vue
@@ -1,25 +1,87 @@
 <template>
     <div>
-        <h2>Step 2: Select Plan</h2>
+        <h2>Select Plan</h2>
+        <div class="plan-options">
+            <div
+            v-for="(plan, index) in plans"
+            :key="index"
+            @click="selectPlan(plan.name)"
+            :class="{ 'selected': selectedPlan === plan.name }"
+            >
+            <h3>{{ plan.name }}</h3>
+            <p>Price: {{ plan.price }} {{ billingCycle }}</p>
+            <p>{{ plan.description }}</p>
+            </div>
+        </div>
+        <div class="billing-toggle">
+            <label class="switch">
+            <input type="checkbox" v-model="isYearly" @change="toggleBillingCycle" />
+            <span class="slider"></span>
+            </label>
+            <span>{{ billingCycle }}</span>
+        </div>
+        <button @click="nextStep" :disabled="!isNextButtonActive">Next</button>
         <button @click="prevStep">Back</button>
-        <button @click="nextStep" :disabled="!isNextEnabled">Next</button>
     </div>
 </template>
 
 <script>
     export default {
+    data() {
+        return {
+            plans: [
+            { name: 'Arcade', price: 10, description: 'Basic plan for arcade gaming.' },
+            { name: 'Advanced', price: 20, description: 'Advanced plan for gaming enthusiasts.' },
+            { name: 'Pro', price: 30, description: 'Professional plan for serious gamers.' },
+            ],
+            selectedPlan: null,
+            isNextButtonActive: false,
+            isYearly: false,
+        };
+    },
+        computed: {
+            billingCycle() {
+                return this.isYearly ? 'Yearly' : 'Monthly';
+            },
+        },
         methods: {
+            selectPlan(planName) {
+                this.selectedPlan = this.selectedPlan === planName ? null : planName;
+                this.checkNextButton();
+                this.saveToLocalStorage();
+            },
+            toggleBillingCycle() {
+                this.checkNextButton();
+                this.saveToLocalStorage();
+            },
+            nextStep() {
+                if (this.isNextButtonActive) {
+                this.$emit('next-step');
+                }
+            },
             prevStep() {
                 this.$emit('prev-step');
             },
-            nextStep() {
-                this.$emit('next-step');
-            }
+            checkNextButton() {
+                this.isNextButtonActive = !!this.selectedPlan;
             },
-            computed: {
-            isNextEnabled() {
-                return true;
-            }
-        }
+            saveToLocalStorage() {
+                localStorage.setItem('selectPlan', JSON.stringify({
+                selectedPlan: this.selectedPlan,
+                isYearly: this.isYearly,
+                }));
+            },
+            loadFromLocalStorage() {
+                const selectPlanData = JSON.parse(localStorage.getItem('selectPlan'));
+                if (selectPlanData) {
+                this.selectedPlan = selectPlanData.selectedPlan;
+                this.isYearly = selectPlanData.isYearly;
+                this.checkNextButton();
+                }
+            },
+        },
+            created() {
+            this.loadFromLocalStorage();
+        },
     };
-</script>  
+</script>
