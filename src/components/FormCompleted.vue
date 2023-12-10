4f3704caeda648b3e5f555bd718165fc4898654f
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-12-10 22:45:57 +0300

    Update
    Updated for FormCompleted.

--- src/components/FormCompleted.vue
+++ src/components/FormCompleted.vue
@@ -1,24 +1,18 @@
 <template>
-    <div>
-        <h2>Completed</h2>
-        <p>Congratulations! You have completed the multi-step form.</p>
-        <button @click="$router.go(-1)">Begin Again</button>
-    </div>
+  <div>
+    <h2>Completed</h2>
+    <p>Congratulations! You have completed the multi-step form.</p>
+    <button @click="restart">Begin Again</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            restart() {
-                this.$emit("set-step", 1);
-        
-                localStorage.removeItem("yourInfo");
-                localStorage.removeItem("selectPlan");
-                localStorage.removeItem("addOns");
-                localStorage.removeItem("summaryinformation");
-        
-                this.$emit("next-step");
-            },
-        },
-    };
+export default {
+  methods: {
+    restart() {
+      localStorage.clear();
+      this.$router.push({ path: '/' });
+    },
+  },
+};
 </script>
