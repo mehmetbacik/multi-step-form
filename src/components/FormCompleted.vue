46c91513f899ab0614980993c64dc58fcb280de5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-01-15 19:55:06 +0300

    Update
    Updated for completed.

--- src/components/FormCompleted.vue
+++ src/components/FormCompleted.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="completed">
-    <span class="headline">Thank you!</span>
-    <p class="text">
-      Thanks for confirming your subscription! We hope you have fun using our
-      platform. If you ever need support, please feel free to email us at
-      support@loremgaming.com.
-    </p>
-    <button class="completed.button" @click="restart">Begin Again</button>
+    <div class="complete-content">
+      <span class="headline">Thank you!</span>
+      <div class="text">
+        Thanks for confirming your subscription! We hope you have <br>fun using our
+        platform. If you ever need support, please feel <br>free to email us at
+        support@loremgaming.com.
+      </div>
+      <div class="button-area">
+        <button class="completed-button" @click="restart">Begin Again</button>
+      </div>
+    </div>
   </div>
 </template>
 
